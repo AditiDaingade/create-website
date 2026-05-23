08e2ff48eed069516da3dfe546d9443ef78a6efc
AditiDaingade <aditidaingade7@gmail.com>
2026-05-23 10:55:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// var manuIcon = document.querySelector(".menu-icon");
+// var sidebar = document.querySelector(".sidebar");
+// var container = document.querySelector(".container");
+
+// manuIcon.onclick = function(){
+//     sidebar.classList.toggle("small-sidebar");
+//     container.classList.toggle(large-container)
+// }
+
+
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuIcon.onclick = function () {
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+};
+
